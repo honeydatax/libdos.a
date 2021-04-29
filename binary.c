8267af9f89af7324ee121d5e80969044e7446e70
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-04-29 19:46:47 +0100

    Add files via upload

---
+++ binary.c
@@ -0,0 +1,98 @@
+// need file libdos.c to be compile in directory
+//bcc -x -i -L -Md hello.c -o HELLO.COM
+#define varn 0x0080
+void sstr(i,s);
+void sputc(cc);
+void sputs(cc);
+int bits(value,n);
+void tobin(value,txt);
+int main(){
+	int k=0;
+	int kk=0;
+	int n=1;
+	char s[80];
+		for (k=0;k<16;k++){
+			tobin(k,s);
+			sputs(s);
+			sputs("\r\n\0$");
+			n=n+n;
+		}
+
+		asm	"db 0xb4,0,0xcd,0x21";
+		
+	return 0;
+}
+
+void sputc(cc)
+char cc;
+{
+	int *c;
+	c = (int * ) varn;
+	*(c + 0) = cc;
+
+	asm	"db 0xbb,0x80,0x0,0x2e,0x8a,0x17,0xb4,0x02,0xcd,0x21";
+}
+void sstr(i,s)
+int i;
+char *s;
+{
+	int n=0;
+	int i10=10;
+	int start=10000;
+	int state=0;
+	int sss=0;
+	int remain=0;
+	int boo=0;
+	char *cursor;
+	char cc;
+	sss=i;
+	cursor=s;
+	
+	for (n=0;n<5;n++){
+		if(n>3)boo=1;
+		state=sss/start;
+		remain=sss-(state*start);
+		sss=remain;
+		cc=(char) state+'0';
+		if(cc!='0')boo=1;
+		if(boo==1)cursor[0]=cc;
+		start=start/i10;
+		if(boo==1)cursor++;
+	}
+	cursor[0]=0;
+}
+
+void sputs(cc)
+char *cc;
+{
+		int i=0;
+while(cc[i]!=0){
+		sputc(cc[i]);
+		i++;
+}
+}
+
+void tobin(value,txt)
+int value;
+char *txt;
+{
+	int n;
+	int i;
+	for(n=0;n<16;n++)txt[n]='0';
+	for(n=0;n<16;n++){
+		i=bits(value,n);
+		if(i==1)txt[15-n]='1';
+	}
+	txt[16]=0;
+	txt[17]='$';
+}
+int bits(value,n)
+int value;
+int n;
+{
+	static unsigned int bitv[]={1,2,4,8,16,32,64,128,512,1024,2048,4096,8192,16384,0x7fff};
+	int nn;
+	nn=value & bitv[n];
+	nn=nn >> n;
+	return nn;
+}
